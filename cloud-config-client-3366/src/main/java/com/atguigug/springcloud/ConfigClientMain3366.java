550959aa8df1da214045123fee262d57e01b6230
sachs <s123158@outlook.com>
2021-02-21 18:51:53 +0800

    SpringCloud config分布式配置中心 SpringCloud Bus消息总线

---
+++ cloud-config-client-3366/src/main/java/com/atguigug/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,15 @@
+package com.atguigug.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import javax.security.auth.login.Configuration;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
